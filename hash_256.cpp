2ff8af7a6c168450d0b61482e9f179b2e93cd731
Rapolas Kairys <rapolas.kairys@gmail.com>
2022-12-09 02:52:37 +0200

    Sukurta string to binary funkcija

--- hash_256.cpp
+++ hash_256.cpp
@@ -1,38 +1,57 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <cmath>
+#include <bitset>
 
 using std::string;
 using std::cout;
 using std::endl;
+string toBinary(int x);
 string hash(string input);
-unsigned int getblock(string block, int blocknr);
 
 int main()
 {
   string input = "";
-  std::cout<<"Iveskite savo string: ";
-  std::cin>>input;
-  std::cout<<"Sugeneruotas hash: "<<hash(input);
+  cout<<"Iveskite savo string: ";
+  std::getline(std::cin, input);
+  cout<<"Sugeneruotas hash: "<<hash(input);
+}
+
+/*prideti 0 gale kad butu 256bitai
+paskutiniai bitai parodo kiek simboliu buvo zinutej
+
+*/
+string toBinary(int x)
+{
+  std::bitset<8> bin_x(x);
+  return bin_x.to_string();
 }
 
 string hash(string input)
 {
-  int x;
-  const int bytesleft = input.length() % 4; //likusiu baitu skaiciu padalinus is 4
-  const int blockamount = (input.length() - bytesleft) / 4; //4 baitu blokai
-  cout<<blockamount<<endl<<bytesleft<<endl;
-  for (int blocknr = 0; blocknr < blockamount; blocknr++)
-  {
-    x = getblock(input, blocknr);
-  }
-  return input;
+  string s = "";
+  string orgLenght = toBinary(input.length()); //reikia padaryti int to binary conversion
+  cout<<input.length()<<endl;
+  for (int i = 0; i < input.length(); i++)
+    {
+        s += toBinary(input[i]);
+    } //convert to binary
+  s += '1'; //pazymeti, nuo kur prasideda pridetiniai nuliai
+  int pow = (s.length() + 64) / 256; // koks 256 laipsnis
+  if(pow < 1) pow++;
+  int apendZeros = (256 * pow) - s.length() - 64;
+
+  s.append(apendZeros, '0'); 
+  cout<<s;
+  return "";
 }
 
-unsigned int getblock(string block, int blocknr) //funkcija paima po 4 elementus, juos pavercia i ascii decimal vertes ir sudeda
-{
-  return	 block[(blocknr * 4)] +
-      (block[(blocknr * 4) + 1]) +
-			(block[(blocknr * 4) + 2]) +
-			(block[(blocknr * 4) + 3]);
-}
+// formatInput(input)
+// {
+//   while(.length() < 223) //32bitai zinutes ilgui
+//   {
+    
+//   }
+
+// }
