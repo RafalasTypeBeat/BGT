0ec9ec225af570600ccc401e239c8e7c3e51b59a
Rapolas Kairys <rapolas.kairys@gmail.com>
2022-12-09 19:08:07 +0200

    pridetas string padalinimas i 32 bitu blokus

--- hash_256.cpp
+++ hash_256.cpp
@@ -3,8 +3,10 @@
 #include <sstream>
 #include <cmath>
 #include <bitset>
+#include <vector>
 
 using std::string;
+using std::vector;
 using std::cout;
 using std::endl;
 string toBinary(int x);
@@ -24,9 +26,9 @@ paskutiniai bitai parodo kiek simboliu buvo zinutej
 */
 string toBinary(int x)
 {
-  cout << x <<endl;
+  //cout << x <<endl;
   std::bitset<8> bin_x(x);
-  cout<<bin_x<< endl;
+  //cout<<bin_x<< endl;
   return bin_x.to_string();
 }
 
@@ -40,12 +42,30 @@ string hash(string input)
         s += toBinary(input[i]);
     } //convert to binary
   s += '1'; //pazymeti, nuo kur prasideda pridetiniai nuliai
-  int pow = (s.length() + 64) / 256; // koks 256 laipsnis
+  int pow = (s.length() + 32) / 256; // koks 256 laipsnis
   if(pow < 1) pow++;
   int apendZeros = (256 * pow) - s.length() - 32; //32bitai saugoti string ilgiui
   s.append(apendZeros, '0'); //padaro 224 bitu ilgio
   s += orgLenght.to_string();
-  cout<<s;
+
+  cout<<s<<endl;
+  
+  int size = pow * 8;
+
+  vector<string> data;
+
+  int index = 0;
+  string buffer;
+
+  for (int i = 1; i <= pow * 8; i++)
+  {
+    buffer.assign(s, index, 32);
+    data.push_back(buffer);
+    index += 32;
+    cout<<data[i - 1]<<" "<<index<<endl;
+  }   //padalina string i 32 bitu blokus
+
+  cout<<endl<<index;
   return "";
 }
 
